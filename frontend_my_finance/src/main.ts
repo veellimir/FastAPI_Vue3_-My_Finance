934f516d14992276506db7927a6ad7ba66f9a42f
vellimir <kkazancov@gmail.com>
2024-08-12 21:42:51 +0400

    connect bootstrap

--- frontend_my_finance/src/main.ts
+++ frontend_my_finance/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import App from './App.vue'
 import router from './router/router';
 
