8526571f82771810ddfcd453cb8b4f7f98bfdb01
vellimir <kkazancov@gmail.com>
2024-08-12 21:34:56 +0400

    fixed config

--- app/core/config.py
+++ app/core/config.py
@@ -55,7 +55,7 @@ class AccessToken(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=(".env.template", ".env"),
+        env_file=(".env-template", ".env"),
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
--- app/main.py
+++ app/main.py
@@ -29,9 +29,6 @@ main_app.include_router(
 
 origins = [
     "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175",
-    "http://127.0.0.1:8000"
 ]
 
 main_app.add_middleware(
